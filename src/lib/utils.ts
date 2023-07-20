d79f4d0ca3de1d8b96781f478c104ca074409ef0
Shajahan A <Shajansheriff@users.noreply.github.com>
2023-07-20 14:07:49 +0530

    add shuffle util

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,12 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+type Shuffle = <T>(arr: T[]) => T[];
+
+export const shuffle: Shuffle = (arr) => {
+  return arr
+    .map((item) => ({ item, sort: Math.random() }))
+    .sort((a, b) => a.sort - b.sort)
+    .map(({ item }) => item);
+};
